15340f1f38741dfeee1aec972af084b42807a0ab
simsimjae <191429@ssg.com>
2019-12-30 15:51:28 +0900

    history api 연습

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import './index.scss';
-import AsyncSplitMe from './ssr/AsyncSplitMe';
-import { Route } from 'react-router-dom';
-import { Home, About, Posts } from './pages';
-import Menu from './components/Menu';
+import React from "react";
+import "./index.scss";
+import AsyncSplitMe from "./ssr/AsyncSplitMe";
+import { Route } from "react-router-dom";
+import { Home, About, Posts, History } from "./pages";
+import Menu from "./components/Menu";
 
 function App() {
 	return (
-		<div className='App'>
+		<div className="App">
 			<Menu></Menu>
 			<AsyncSplitMe></AsyncSplitMe>
-			<Route exact path='/' component={Home}></Route>
-			<Route path='/about/:name?' component={About}></Route>
-			<Route path='/posts' component={Posts}></Route>
+			<Route exact path="/" component={Home}></Route>
+			<Route path="/about/:name?" component={About}></Route>
+			<Route path="/posts" component={Posts}></Route>
+			<Route path="/history" component={History}></Route>
 		</div>
 	);
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
 const Menu = () => {
 	const activeStyle = {
-		color: 'red',
-		fontSize: '2rem'
+		color: "red",
+		fontSize: "2rem"
 	};
 	return (
 		<div>
-			<NavLink exact to='/' activeStyle={activeStyle}>
+			<NavLink exact to="/" activeStyle={activeStyle}>
 				홈
 			</NavLink>
-			<NavLink exact to='/about' activeStyle={activeStyle}>
+			<NavLink exact to="/about" activeStyle={activeStyle}>
 				소개
 			</NavLink>
-			<NavLink exact to='/about/react' activeStyle={activeStyle}>
+			<NavLink exact to="/about/react" activeStyle={activeStyle}>
 				리액트 소개
 			</NavLink>
-			<Link to='/posts'>포스트</Link>
+			<Link to="/posts">포스트</Link>
+			<Link to="/history">히스토리API</Link>
 		</div>
 	);
 };
