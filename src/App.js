ffa88aa070b4fd87424e9e552b6815fdf3059612
simsimjae <191429@ssg.com>
2020-01-21 16:23:04 +0900

    동적 Context API 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from "react";
 import "./index.scss";
 import ColorBox from "./components/ColorBox";
-import ColorContext from "./contexts/color"; // 색상 저장 창고 가져옴.
+import ColorContext, { ColorProvider } from "./contexts/color"; // 색상 저장 창고 가져옴.
+import SelectColors from "./components/SelectColors";
 
 function App() {
 	return (
 		<div className="App">
 			<h2>Context API 사용하기</h2>
 
-			<ColorContext.Provider value={{ color: "red" }}>
+			<ColorProvider>
 				<div>
+					<SelectColors />
 					<ColorBox />
 				</div>
-			</ColorContext.Provider>
-
-			<p>원래 색상 저장 창고에는 검정색이 들어있었다. 그런데, 판매자 Provider씨는 color를 red로 바꿔버렸다.</p>
-			<p>판매자가 갑이기 때문에 색깔을 빨간색으로 바꾸면 구매자는 그 바뀐 빨간색을 살 수 밖에없다.</p>
-			<p>여기서 소비자는 ColorBox이다. 판매자는 ColorContext.Provider이다.</p>
-			<p>Provider가 변경한 정보(value={}부분)는 Provider의 자식들(ColorBox)에게 모두 영향을 미친다.</p>
+			</ColorProvider>
 		</div>
 	);
 }
--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,10 +1,17 @@
 import React from "react";
-import ColorContext from "../contexts/color";
+import ColorContext, { ColorConsumer } from "../contexts/color";
 
 const ColorBox = () => {
-	// 저장창고에 들어있는 색상을 꺼내온다.
-	// Consumer(소비자)는 Provider(판매자)가 저장창고에서 꺼낸 색상을(기본은 검정색인데, 판매자가 방금 빨간색으로 바꿈) 구매해서 박스에 페인트칠을 한다.
-	return <ColorContext.Consumer>{({ color }) => <div style={{ width: "64px", height: "64px", background: color }}></div>}</ColorContext.Consumer>;
+	return (
+		<ColorConsumer>
+			{context => (
+				<>
+					<div style={{ width: "64px", height: "64px", background: context.state.color }}></div>
+					<div style={{ width: "32px", height: "32px", background: context.state.subColor }}></div>
+				</>
+			)}
+		</ColorConsumer>
+	);
 };
 
 export default ColorBox;
