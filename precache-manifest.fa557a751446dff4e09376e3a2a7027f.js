6192f9c66d32c8bcc67259c6db206add557986fd
simsimjae <191429@ssg.com>
2020-02-04 10:19:31 +0900

    Updates

---
+++ precache-manifest.fa557a751446dff4e09376e3a2a7027f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80af195887475f06472628b45df3db4c",
+    "url": "/react_todolist/index.html"
+  },
+  {
+    "revision": "f7b99485c35a4407db60",
+    "url": "/react_todolist/static/css/main.c9580bbf.chunk.css"
+  },
+  {
+    "revision": "1d1f679d703d6d100f40",
+    "url": "/react_todolist/static/js/2.59d80222.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react_todolist/static/js/2.59d80222.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7b99485c35a4407db60",
+    "url": "/react_todolist/static/js/main.b0d451b7.chunk.js"
+  },
+  {
+    "revision": "e88b77cae2efce4ecfee",
+    "url": "/react_todolist/static/js/runtime-main.b1947308.js"
+  }
+]);
